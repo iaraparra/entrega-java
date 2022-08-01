30efdcbea9f37caf72be98ee3407f0f1e4f7b868
iaraparra <iaraguada02@gmail.com>
2022-08-01 17:16:28 -0300

    nuevo footer y funcion finalizar compra

--- js/mainCarrito.js
+++ js/mainCarrito.js
@@ -1,16 +1,26 @@
+function compraExitosa(){
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Compra Exitosa',
+    showConfirmButton: false,
+    timer: 1500
+  })
+}
+
 function renderProductosCarrito() {
     let productos = obtenerProductosCarrito();
     let contenido = `<p class="alert alert_purple text-center" role="alert">No se encontraron productos en el carrito.</p>`
   
     if (productos.length > 0) {
-      contenido = `<p class="text-end"><a href="#" class="btn btn_purple button text-black" onclick="vaciarCarrito()" title="Vaciar Carrito">Vaciar Carrito <img src= "assets/imagenes/delete.png" width="24"></a></p>
+      contenido = `<p class="text-end"><a href="#" class="btn btn_purple button text-black botonVaciar" onclick="vaciarCarrito()" title="Vaciar Carrito">Vaciar Carrito <img src= "assets/imagenes/delete.png" width="24"></a></p>
         <table class="table">`;
         let total =0;
   
       for (let producto of productos) {
         let valor = producto.valor * producto.cantidad;
         contenido += ` <tr>
-          <td><img src="${producto.imagen}" alt="${producto.nombre}" width="64"></td>
+          <td><img src="${producto.imageURL}" alt="${producto.nombre}" width="64"></td>
           <td class="align-middle">${producto.nombre} X ${producto.cantidad}</td>
           <td class="align-middle"><b>$${valor}</b></td>
           <td class="text-end"><a href="#" class="btn btn button" onclick="eliminarCarrito(${producto.id})" ><img src="assets/imagenes/delete.png" width="24"></a> </td>
@@ -23,7 +33,7 @@ function renderProductosCarrito() {
       <td>&nbsp;</td>
       <td>Total a Pagar</td>
       <td class="align-middle"><b>$${total}</b></td>
-      <td class="text-end"><a href="#" class="btn btn_purple">Finalizar Compra</a></td>
+      <td class="text-end"><a href="#" class="btn btn_purple botonFinalizar" onclick="compraExitosa()">Finalizar Compra</a></td>
       </tr>`;
   
       contenido += `</table>`;
